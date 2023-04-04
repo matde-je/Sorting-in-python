7d64befe78fd6a1b53eea74b4e7e3c637371a6f6
matde-je <matidlecosta719@gmail.com>
2023-04-04 16:38:07 +0100

    upload files

---
+++ sorting.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+import numpy
+
+def partitions(data, n): #dividir em numeros mais pequenos para facilitar a funçao sorting #número de observações por partição.
+
+
+
+
+
+def sorting(list, ascending): #devolve o nr de iterações para ordenar e o mínimo e o máximo
+    #utilizar quicksort
+    
+def execute(data, n, ascending)
+
+
+
+
